341d98eaa51c23a3df19bcc247399604bdfb8df2
Hari Kunwar <harikunwar70@gmail.com>
2020-11-01 04:54:54 -0800

    Empty project files.

---
+++ SimpleCoordinator/SimpleCoordinator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleCoordinator
+//
+//  Created by Kunwar, Hari on 11/1/20.
+//  Copyright © 2020 Learning. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
